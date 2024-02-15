ac882765078b66967b17b930deba29159438a9e3
S672672 <bsmith672@rku.ac.in>
2024-02-15 18:54:51 +0530

    navitems

--- src/Components/NavTitle.jsx
+++ src/Components/NavTitle.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function NavTitle({NavTitle}) {
   return (
-    <div className='h-10 border-br-4'>
-    <div className='text-bold text-black text-2xl ml-8'>{NavTitle}</div>
+    <div className='h-20 border-b-4 border-black flex items-center'>
+    <div className='text-bold text-black text-3xl ml-8'>{NavTitle}</div>
     </div>
   )
 }
