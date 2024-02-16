7326e2a70f14f229f27c5a99a32e8c3878e76791
S672672 <bsmith672@rku.ac.in>
2024-02-16 20:55:27 +0530

    all items added

--- src/Components/ShowAll.jsx
+++ src/Components/ShowAll.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
-export default function ShowAll() {
+export default function ShowAll({title,amount,share,ShowIcon}) {
   return (
-    <div>ShowAll</div>
+    <div className='w-3/12 h-24 bg-white flex items-center justify-between rounded-md px-4 my-10 mx-5'>
+    <div className='flex flex-col'>
+    <div className='font-bold text-gray-400 text-xl'>{title}</div>
+    <div className='flex gap-2'>
+        <div className='font-bold text-black text-2xl'>{amount}</div>
+        <div className='bg-green-200 text-bold text-black'>{share}</div>
+    </div>
+    </div>
+    <img src={ShowIcon} alt='show'></img>
+    </div>
   )
 }
