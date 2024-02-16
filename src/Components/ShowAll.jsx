2030c5433dcabc2b2c08d45d861e0071185caa35
S672672 <bsmith672@rku.ac.in>
2024-02-16 20:42:09 +0530

    items added

---
+++ src/Components/ShowAll.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ShowAll() {
+  return (
+    <div>ShowAll</div>
+  )
+}
--- src/ViewPage.jsx
+++ src/ViewPage.jsx
@@ -4,7 +4,7 @@ import DashBoard from './DashBoard'
 
 function ViewPage() {
   return (
-    <div className='flex flex-col w-screen bg-gray-200'>
+    <div className='flex flex-col w-screen bg-gray-100'>
     <Header />
     <DashBoard />
     </div>
