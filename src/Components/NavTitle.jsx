654341653da2e4f4cc15cfbbf3c8f2824abc3139
S672672 <bsmith672@rku.ac.in>
2024-02-16 21:05:13 +0530

    just a small change on the components of showitems

--- src/Components/NavTitle.jsx
+++ src/Components/NavTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function NavTitle({NavTitle}) {
   return (
-    <div className='h-20 border-b-2 border-black flex items-center'>
+    <div className='h-20 border-b-2 border-gray-300 flex items-center'>
     <div className='text-bold text-black text-3xl ml-8'>{NavTitle}</div>
     </div>
   )
--- src/Components/ShowAll.jsx
+++ src/Components/ShowAll.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-export default function ShowAll({title,amount,share,ShowIcon}) {
+export default function ShowAll({ title, amount, share, ShowIcon }) {
   return (
-    <div className='w-3/12 h-24 bg-white flex items-center justify-between rounded-md px-4 my-10 mx-5'>
-    <div className='flex flex-col'>
-    <div className='font-bold text-gray-400 text-xl'>{title}</div>
-    <div className='flex gap-2'>
-        <div className='font-bold text-black text-2xl'>{amount}</div>
-        <div className='bg-green-200 text-bold text-black'>{share}</div>
+    <div className='w-full sm:w-1/2 md:w-1/3 lg:w-3/12 xl:w-3/12 h-24 bg-white flex items-center justify-between rounded-md px-4 my-2 mx-2 md:mx-4 lg:mx-6 xl:mx-8 flex-wrap shadow-md'>
+      <div className='flex flex-col'>
+        <div className='font-bold text-gray-400 text-lg md:text-xl'>{title}</div>
+        <div className='flex gap-2'>
+          <div className='font-bold text-black text-xl md:text-2xl'>{amount}</div>
+          <div className='bg-green-200 text-bold text-black'>{share}</div>
+        </div>
+      </div>
+      <img className='w-8 h-8 md:w-10 md:h-10' src={ShowIcon} alt='show' />
     </div>
-    </div>
-    <img src={ShowIcon} alt='show'></img>
-    </div>
-  )
+  );
 }
