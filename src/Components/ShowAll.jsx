b536a3780aa67ca2c9526880f820427948fbf3a0
S672672 <bsmith672@rku.ac.in>
2024-02-18 18:32:00 +0530

    a little change on the dashboard

--- src/Components/ShowAll.jsx
+++ src/Components/ShowAll.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ShowAll({ title, amount, share, ShowIcon }) {
   return (
-    <div className='w-full sm:w-1/2 md:w-1/3 lg:w-3/12 xl:w-3/12 h-24 bg-white flex items-center justify-between rounded-md px-4 my-2 mx-2 md:mx-4 lg:mx-6 xl:mx-8 flex-wrap shadow-md'>
+    <div className='w-full sm:w-1/2 md:w-1/3 lg:w-3/12 xl:w-3/12 h-24 bg-white flex items-center justify-between rounded-md px-4 my-2 mx-2 md:mx-4 lg:mx-6 xl:mx-8 flex-wrap shadow-md hover:bg-gray-100 cursor-pointer'>
       <div className='flex flex-col'>
         <div className='font-bold text-gray-400 text-lg md:text-xl'>{title}</div>
         <div className='flex gap-2'>
