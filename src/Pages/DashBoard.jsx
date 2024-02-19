1eef9c38a43272edbb34894143427cb9991eeb8a
S672672 <bsmith672@rku.ac.in>
2024-02-19 15:01:13 +0530

    Pages and components fiexed

--- src/Pages/DashBoard.jsx
+++ src/Pages/DashBoard.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import NavTitle from './Components/NavTitle'
-import ShowAll from './Components/ShowAll'
+import ShowAll from '../Components/ShowAll'
 
 function DashBoard() {
   return (
     <div className='flex flex-col m-3'>
-    <NavTitle NavTitle="DashBoard" />
     <ShowAll title = "Total" amount="$30,300" share="+2.4%" ShowIcon="" />
     </div>
   )
--- src/ViewPage.jsx
+++ src/ViewPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header'
-import DashBoard from './DashBoard'
+import DashBoard from './Pages/DashBoard'
+import NavTitle from './Components/NavTitle'
 
 function ViewPage() {
   return (
     <div className='flex flex-col w-screen bg-gray-100'>
     <Header />
+    <NavTitle NavTitle="DashBoard" />
     <DashBoard />
     </div>
   )
