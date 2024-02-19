8c4c44e9929ced89ad0cd8b2863129d4416a391e
S672672 <bsmith672@rku.ac.in>
2024-02-19 14:51:42 +0530

    router installed

--- src/DashBoard.jsx
+++ src/DashBoard.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import NavTitle from './Components/NavTitle'
 import ShowAll from './Components/ShowAll'
-import BarChart from './Components/BarChart'
 
 function DashBoard() {
   return (
     <div className='flex flex-col m-3'>
     <NavTitle NavTitle="DashBoard" />
     <ShowAll title = "Total" amount="$30,300" share="+2.4%" ShowIcon="" />
-    <BarChart />
     </div>
   )
 }
