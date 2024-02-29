fad3e91d7cf2e175fa9e4a6c8fb13d8facfd6141
S672672 <bsmith672@rku.ac.in>
2024-02-29 15:24:26 +0530

    search

--- src/Components/NotificationsSector.jsx
+++ src/Components/NotificationsSector.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-function NotificationsSector({ name, activity, timestamp }) {
+export default function NotificationsSector({ name, activity, timestamp }) {
     return (
       <div className='w-full bg-white p-2 px-3 br-2 flex items-center cursor-pointer hover:bg-gray-100'>
         <img
           src='../src/assets/profile.png'
           alt={`${name}'s profile`}
-          className='w-12 h-12 rounded-full mr-4'
+          className='w-20 h-20 rounded-full mr-4'
         />
         <div className='flex-grow'>
           <div className='flex items-center justify-between mb-2'>
             <div>
-              <p className='text-lg font-bold'>{name}</p>
+              <p className='text-xl font-bold'>{name}</p>
               <p className='text-gray-500 font-bold'>{activity}</p>
             </div>
           </div>
@@ -20,5 +20,3 @@ function NotificationsSector({ name, activity, timestamp }) {
       </div>
     );
   }
-  
-  export default NotificationsSector;
