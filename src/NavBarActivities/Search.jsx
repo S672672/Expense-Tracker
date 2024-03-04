83773eda003f1d017b9e52b2839a67eb78e0cb54
S672672 <bsmith672@rku.ac.in>
2024-03-04 20:18:35 +0530

    setting on progress

--- src/NavBarActivities/Search.jsx
+++ src/NavBarActivities/Search.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Search() {
+export default function Search() {
   return (
     <div className='fixed top-16 right-4 z-50 w-1/5 max-h-[95vh] shadow-3xl shadow-gray-400 bg-white p-2 mb-8 my-1 mr-3'>
       <div className='h-24 w-full flex items-center justify-between'>
@@ -14,5 +14,3 @@ function Search() {
     </div>
   )
 }
-
-export default Search
--- src/NavBarActivities/Settings.jsx
+++ src/NavBarActivities/Settings.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
-function Settings() {
+export default function Settings() {
   return (
     <div>Settings</div>
   )
 }
-
-export default Settings
